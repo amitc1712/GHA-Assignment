53dcb04d41fd1e4d3f9d063e39ed07ab0acb952e
amitc1712 <148864478+amitc1712@users.noreply.github.com>
2023-11-22 12:53:11 +0530

    Add files via upload

---
+++ tests/test_general_example.py
@@ -0,0 +1,31 @@
+import unittest
+from unittest.mock import patch
+from general_example import GeneralExample
+
+general_example_instance = GeneralExample()
+
+class TestEmployeeRecordLoading(unittest.TestCase):
+
+    def test_flatten_dictionary(self):
+        Dict = {'key1': [3, 2, 1], 'key2': [42, 55, 10], 'key3': [0, 22]}
+        expected = [0, 1, 2, 3, 10, 22, 42, 55]
+
+        actual = general_example_instance.flatten_dictionary(Dict)
+
+        assert expected == actual
+
+    @patch('general_example.GeneralExample.load_employee_rec_from_database')
+    def test_fetch_emp_details(self, mock_load_employee_rec):
+        mock_load_employee_rec.return_value = ['emp001', 'Sam', '100000']
+
+        emp_details = general_example_instance.fetch_emp_details()
+
+        expected_emp_details = {
+            'empId': 'emp001',
+            'empName': 'Sam',
+            'empSalary': '100000'
+        }
+        self.assertEqual(emp_details, expected_emp_details)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_tdd_example.py
@@ -0,0 +1,38 @@
+import unittest
+from tdd_example import TDDExample
+tdd_example_instance = TDDExample()
+
+class TestTdd(unittest.TestCase):
+    
+    def test_reverse_string(self):
+        text = 'foobar'
+        expected = 'raboof'
+
+        actual = tdd_example_instance.reverse_string(text)
+
+        assert expected == actual
+
+    def test_find_longest_word(self):
+        sentence = '''
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        '''
+        expected = 'consectetur'
+        actual = tdd_example_instance.find_longest_word(sentence)
+
+        assert expected == actual
+
+    def test_reverse_list(self):
+        expected = [1, 2, 3, 4, 5]
+        actual = tdd_example_instance.reverse_list([5, 4, 3, 2, 1])
+
+        assert expected == actual
+
+    def test_count_digits(self):
+        expected = 3
+
+        actual = tdd_example_instance.count_digits([1, 1, 1, 2, 3], 1)
+
+        assert expected == actual
+
+if __name__ == '__main__':
+    unittest.main()
